0683c4410281ccca850d089342788a272d430cb3
zhangtong <1234567890>
2021-06-25 17:58:23 +0800

    Initial commit

---
+++ src/main/java/com/example/mysqlmanage/MysqlManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqlmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mysqlmanage/MysqlManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqlmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
